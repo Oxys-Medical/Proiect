f3f34db7c0c5fd8e8aa9355c1c37daf26bdc7002
Hannelore Ghisan <84455824+RealHanne@users.noreply.github.com>
2021-07-28 16:39:54 +0300

    Data Layer update
    am lesinat putin
    
    Co-Authored-By: Briscan Roxana <84455937+RoxanaBriscan@users.noreply.github.com>

--- OXYs/DataLayer.cpp
+++ OXYs/DataLayer.cpp
@@ -1,6 +1,6 @@
 #include "StorageLayer.h"
 #include "DataLayer.h"
-
+#include "PulseOxySampler.h"
 
 
 DataLayer::DataLayer()
@@ -10,9 +10,38 @@ DataLayer::DataLayer()
 
 void DataLayer::AddPatientData(long CNP)
 {
-    // verificari ca este CNP valid, daca mai exista deja sau nu, etc..
-    _storageLayer.AddPatientData(CNP);
+    if(isValid(CNP))
+        if(iSNew(CNP))
+            //o sa fie trecut in cnp.txt se va crea o adresa la txt nou si apoi se va crea un txt nou
+            
+              _storageLayer.AddMeasurement(actualPulse, actualSaturation, currentDataTime)
+             _storageLayer.AddPatientData(CNP);
+
+        else 
+            //scriem in cnp.txt adresa noua si creaza un txt nou 
+             _storageLayer.AddMeasurement(actualPulse, actualSaturation, currentDataTime)
+             _storageLayer.AddPatientData(CNP);
+    else 
+        //se intoarce in data entrystate? 
+
+    
 }
+
+bool isValid(long CNP)
+{
+    if((CNP%10^13 == 0)   &&    (CNP%10^12 == 5 || CNP%10^12 == 6 CNP%10^12 == 1 || CNP%10^12 ==2 )
+        return 0;
+    else 
+        return 1;
+}
+// bool isNew(long CNP)
+// {
+//
+//     if(CNP != memoryCNP)
+//         return 0; 
+//     else
+//         return 1;
+// }
 
 void DataLayer::AddMeasurement(int actualPulse, int actualSaturation)
 {
