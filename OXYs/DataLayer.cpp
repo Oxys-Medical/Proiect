7b9515b459ab8195f86f10e33f6df4c108aa48d5
Hannelore Ghisan <84455824+RealHanne@users.noreply.github.com>
2021-08-04 12:31:39 +0300

    Update DataLayer.cpp

--- OXYs/DataLayer.cpp
+++ OXYs/DataLayer.cpp
@@ -3,7 +3,7 @@
 #include "PulseOxySampler.h"
 #include "PulseOxyFunctions.h"
 #include <SPI.h>
-#include <SD.h> //trebuie pentru card
+#include <SD.h>    //trebuie pentru card
 #include <fstream> // trebuie la fisiere
 
 DataLayer::DataLayer()
@@ -16,34 +16,32 @@ void DataLayer::AddPatientData(long CNP, int actualPulse, int actualSaturation, 
     if (isValid(CNP))
         if (iSNew(CNP))
             //o sa fie trecut in cnp.txt se va crea o adresa la txt nou si apoi se va crea un txt nou
-         _storageLayer.AddPatientData(CNP);
-         _storageLayer.AddMeasurement(actualPulse, actualSaturation, currentDataTime)
+            _storageLayer.AddPatientData(CNP);
+    _storageLayer.AddMeasurement(actualPulse, actualSaturation, currentDataTime)
 
         else
         //scriem in cnp.txt adresa noua si creaza un txt nou
-         _storageLayer.AddMeasurement(actualPulse, actualSaturation, currentDataTime)
-         _storageLayer.AddPatientData(CNP);
+        _storageLayer.AddMeasurement(actualPulse, actualSaturation, currentDataTime)
+            _storageLayer.AddPatientData(CNP);
 
     else
-    
+
     //nu face nimic
 }
 void DataLayer::AppendDigit(long currentCNP, int x)
 {
-    currentCNP=currentCNP*10+x;
+    currentCNP = currentCNP * 10 + x;
     //oare asa aduagam?
-    
 }
 void DataLayer::DeleteDigit(long currentCNP, int x)
 {
-    currentCNP = (currentCNP - x)/10;  
+    currentCNP = (currentCNP - x) / 10;
     //asa stergem?
 }
 void DataLayer::ConfirmCNP(long currentCNP)
 {
     long CNP = currentCNP;
     //ii stocat undeva in memoria interna
-
 }
 void DataLayer::AddMeasurement(int actualPulse, int actualSaturation)
 {
