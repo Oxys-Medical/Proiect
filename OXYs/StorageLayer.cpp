fb2dcfc3ae0d8c6cbda40bf1e46ff8cb950ffda9
Hannelore Ghisan <84455824+RealHanne@users.noreply.github.com>
2021-09-16 12:15:54 +0300

    Update StorageLayer.cpp

--- OXYs/StorageLayer.cpp
+++ OXYs/StorageLayer.cpp
@@ -8,39 +8,38 @@
 
 // cod pentru SD in arduino
 
-void StorageLayer::AddPatientData(char* CNP, char* currentDateTime)
+void StorageLayer::AddPatientData(char *CNP, char *currentDateTime)
 {
-    setup();
-    CNPFile = SD.open("CNP.txt", FILE_WRITE); 
-    CNPFile.println(CNP);
-    CNPFile.close();
+  SD.begin(CSpin);
+  CNPFile = SD.open("CNP.txt", FILE_WRITE);
+  CNPFile.println(CNP);
+  CNPFile.close();
 }
-void StorageLayer::AddMeasurement(char* CNP, int actualPulse, int actualSaturation, char* currentDataTime)
+void StorageLayer::AddMeasurement(char *CNP, int actualPulse, int actualSaturation, char *currentDataTime)
 {
-    setup();
-    char EndCNP[7];
+  SD.begin(CSpin);
+   char EndCNP[7];
   for (int i = 0; i < 6; i++)
   {
     EndCNP[i] = CNP[i + 7];
   }
-   strcat(EndCNP, ".txt");
-  
-    XFile = SD.open(EndCNP, FILE_WRITE);
-    XFile.print("Datele pentru pacientul cu CNP-ul:");
-    XFile.print(CNP);
-    XFile.println(" sunt:");
-    XFile.println("Data inregistrarilor:");
-    XFile.println(currentDataTime);
-    XFile.print("Pulsul pacientului este: ");
-    XFile.print(actualPulse);
-    XFile.println(" BPM");
-    XFile.print("Saturatie de oxigen a pacientuluin este: ");
-    XFile.print(actualSaturation);
-    XFile.println(" SpO2");
-    XFile.close();
-   
+  strcat(EndCNP, ".txt");
+
+  XFile = SD.open(EndCNP, FILE_WRITE);
+  XFile.print("Datele pentru pacientul cu CNP-ul:");
+  XFile.print(CNP);
+  XFile.println(" sunt:");
+  XFile.println("Data inregistrarilor:");
+  XFile.println(currentDataTime);
+  XFile.print("Pulsul pacientului este: ");
+  XFile.print(actualPulse);
+  XFile.println(" BPM");
+  XFile.print("Saturatie de oxigen a pacientuluin este: ");
+  XFile.print(actualSaturation);
+  XFile.println(" SpO2");
+  XFile.close();
 }
 // int StorageLayer::PatientData()
 // {
-    
+
 // }
