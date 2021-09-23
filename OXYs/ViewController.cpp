5f4b2f26eebdef2f85e2ed8c483e943766b0cac7
Briscan Roxana <84455937+RoxanaBriscan@users.noreply.github.com>
2021-09-23 12:09:03 +0300

    nou

--- OXYs/ViewController.cpp
+++ OXYs/ViewController.cpp
@@ -1,56 +1,53 @@
-#include "BaseView.h"
-#include "InitializingView.h"
-#include "DataInputView.h"
-#include "MeasurementView.h"
-#include "ProblemView.h"
-#include "ReviewView.h"
+//#include "BaseView.h"
+//#include "InitializingView.h"
+//#include "DataInputView.h"
+//#include "MeasurementView.h"
+//#include "ProblemView.h"
+//#include "ReviewView.h"
 #include "ViewController.h"
-#include "Constants.h"
-#include "Commands.h"
-#include "UiButton.h"
-#include "UiElement.h"
+//#include "Constants.h"
+//#include "Commands.h"
+//#include "UiButton.h"
+//#include "UiElement.h"
 
-void UiElement::Animate(){}
+//void UiElement::Animate(){}
 
 ViewController::ViewController()
 {
-    _stateMachine = StateMachine();
-    _displayDriver = DisplayDriver();
-    _displayDriver.Initialize();
-
-    _viewArray = new BaseView[NumberOfViews];
-
-    InitializingView initializingView = InitializingView();
-    _viewArray[InitializingViewIndex] = initializingView;
-    DataInputView dataInputView = DataInputView(_displayDriver, _stateMachine);
-    _viewArray[DataInputViewIndex] = dataInputView;
-    MeasurementView measurementView = MeasurementView(_displayDriver, _stateMachine);
-    _viewArray[MeasurementViewIndex] = measurementView;
-    ProblemView problemView = ProblemView(_displayDriver, _stateMachine);
-    _viewArray[ProblemViewIndex] = problemView;
-    ReviewView reviewView = ReviewView(_displayDriver, _stateMachine);
-    _viewArray[ReviewViewIndex] = reviewView;
-
-    _currentViewIndex = 99; //trebe initializat undeva
-    UpdateView(InitializingViewIndex); //trebuie facuta functie update view
+//    _stateMachine = StateMachine();
+//    _displayDriver = DisplayDriver();
+//    _displayDriver.Initialize();
+//
+//    _viewArray = new BaseView[NumberOfViews];
+//
+//    InitializingView initializingView = InitializingView();
+//    _viewArray[InitializingViewIndex] = initializingView;
+//    DataInputView dataInputView = DataInputView(_displayDriver, _stateMachine);
+//    _viewArray[DataInputViewIndex] = dataInputView;
+//    MeasurementView measurementView = MeasurementView(_displayDriver, _stateMachine);
+//    _viewArray[MeasurementViewIndex] = measurementView;
+//    ProblemView problemView = ProblemView(_displayDriver, _stateMachine);
+//    _viewArray[ProblemViewIndex] = problemView;
+//    ReviewView reviewView = ReviewView(_displayDriver, _stateMachine);
+//    _viewArray[ReviewViewIndex] = reviewView;
+//
+//    _currentViewIndex = 99; //trebe initializat undeva
+//    UpdateView(InitializingViewIndex); //trebuie facuta functie update view
 };
-
 void ViewController :: HandleCommand() 
-{
-    int* contactPoint = _displayDriver.GetContactPoint();
-    byte nextViewIndex = _currentView.HandleCommand(contactPoint);
-    UpdateView(nextViewIndex);
+
+{Serial.println("Roxana  afost aici");
+//    int* contactPoint = _displayDriver.GetContactPoint();
+//    byte nextViewIndex = _currentView.HandleCommand(contactPoint);
+//    UpdateView(nextViewIndex);
 }
 
 void ViewController :: UpdateView(byte viewIndex) //am facut bine? E bine, boss.
 {
-    if (viewIndex != _currentViewIndex)
-    {
-        _currentViewIndex = viewIndex;
-        _currentView = _viewArray[viewIndex];
-        _currentView.Display();
-    }
-}
- void BaseView::Display(){
-
+//    if (viewIndex != _currentViewIndex)
+//    {
+//        _currentViewIndex = viewIndex;
+//        _currentView = _viewArray[viewIndex];
+//        _currentView.Display();
+//    }
 }
--- OXYs/ViewController.h
+++ OXYs/ViewController.h
@@ -1,20 +1,20 @@
 #ifndef __VIEWCONTROLLER_H__
 #define __VIEWCONTROLLER_H__
 
-#include "StateMachine.h"
-#include "DisplayDriver.h"
-#include "BaseView.h"
+//#include "StateMachine.h"
+//#include "DisplayDriver.h"
+//#include "BaseView.h"
 #include <Arduino.h>
 
 
 class ViewController
 {
     private:
-    DisplayDriver _displayDriver;
-    StateMachine _stateMachine; 
-    BaseView _currentView;
-    BaseView* _viewArray;
-    byte _currentViewIndex;
+//    DisplayDriver _displayDriver;
+//    StateMachine _stateMachine; 
+//    BaseView _currentView;
+//    BaseView* _viewArray;
+//    byte _currentViewIndex;
 
     public:
     ViewController();
